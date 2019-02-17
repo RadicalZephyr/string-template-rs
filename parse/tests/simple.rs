fee22cc38582c904abb1729b7a1771dc1a29ed4d
Geoff Shannon <geoffpshannon@gmail.com>
2019-02-16 16:51:05 -0800

    Change group name in template test

--- parse/tests/simple.rs
+++ parse/tests/simple.rs
@@ -1,14 +1,14 @@
 use parse::st_group;
 
 st_group! {
-    static ref simple_bracket {
+    static ref simple_group {
          a() ::= <<foo>>;
-     }
+    }
 }
 
 #[test]
 fn parse_literal_group() {
-    let _a = simple_bracket
+    let _a = simple_group
         .get("a")
         .expect("unexpectedly failed to find template a");
 }
