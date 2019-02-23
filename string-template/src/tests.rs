bf99ef601f17c51767c9352cd252d99d754ef703
Geoff Shannon <geoffpshannon@gmail.com>
2019-02-22 17:20:33 -0800

    Move lib tests into a separate file

---
+++ string-template/src/tests.rs
@@ -0,0 +1,66 @@
+use super::*;
+
+fn parse_template(template: &'static str) -> Template {
+    template
+        .parse::<CompiledTemplate>()
+        .expect("unexpectedly failed to parse template")
+        .into()
+}
+
+#[test]
+fn renders_hello_world() {
+    let mut hello = parse_template("Hello, <name>!");
+    hello.add("name", "World");
+    assert_eq!("Hello, World!", format!("{}", hello.render()));
+}
+
+#[test]
+fn renders_multiple_attributes() {
+    let mut hello = parse_template("Hello, <title><name>!");
+    hello.add("name", "World");
+    hello.add("title", "Old ");
+    assert_eq!("Hello, Old World!", format!("{}", hello.render()));
+}
+
+#[test]
+fn renders_missing_attributes_as_empty_string() {
+    let mut hello = parse_template("Hello, <title><name>!");
+    hello.add("name", "World");
+    assert_eq!("Hello, World!", format!("{}", hello.render()));
+}
+
+fn parse_group(group: &'static str) -> Group {
+    match group.parse() {
+        Ok(group) => group,
+        Err(error) => panic!("unexpectedly failed to parse Group: {}", error),
+    }
+}
+
+fn get_template(group: &Group, name: &'static str) -> Template {
+    group
+        .get(name)
+        .expect(&format!("unexpectedly failed to get template {}", name))
+}
+
+#[test]
+fn renders_template_from_group() {
+    let group = parse_group(
+        r#"
+a() ::= "FOO"
+"#,
+    );
+    let a = get_template(&group, "a");
+    assert_eq!("FOO", a.render());
+}
+
+#[test]
+fn renders_template_with_attribute_from_group() {
+    let group = parse_group(
+        r#"
+a(x) ::= "FOO<x>"
+"#,
+    );
+    let mut a = get_template(&group, "a");
+    a.add("x", "BAR");
+    assert_eq!("FOOBAR", a.render());
+}
